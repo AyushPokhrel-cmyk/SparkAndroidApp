63fd8808e73304b86425535b0c43698cda3772a1
Ayush Pokhrel <ap0190289@gmail.com>
2025-10-17 14:33:53 -0500

    first add

--- app/src/main/java/com/example/sparkinstitute/MainActivity.kt
+++ app/src/main/java/com/example/sparkinstitute/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.sparkinstitute
 
-import android.R.attr.button
+
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
